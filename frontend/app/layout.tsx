a96892e57e9694812ed2fe305ba7ef19431bf839
Israel Guido <israel.guido@bis2bis.com.br>
2025-03-10 16:57:19 -0300

    :pray: new frontend with chakra ui

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,11 +1,18 @@
+'use client'
+
 import { ChakraProvider } from '@chakra-ui/react';
 import { ReactNode } from 'react';
+import Navbar from './components/Navbar';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <ChakraProvider>{children}</ChakraProvider>
+        
+        <ChakraProvider>
+          <Navbar />
+          {children}
+        </ChakraProvider>
       </body>
     </html>
   );
